4192c5fc12144eda1533a13b510e779edf39d8c4
Mark Brown <markb1994@icloud.com>
2017-12-14 14:58:58 +0000

    Added functionality for login/logout
    Login & Logout allow users to now tap in and turn on ted. Ted now has a
    heartbeat that is visual and touch qualities. Force sensitive resistor
    has also been modified to change the interval time to a more life like
    range
    
    :bear:

--- killerTed_master/killerTed_master.ino
+++ killerTed_master/killerTed_master.ino
@@ -1,18 +1,32 @@
+//Viral Science
+//RFID
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Servo.h>
 #include <Wire.h>
 #include "Adafruit_DRV2605.h"
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+
+Adafruit_DRV2605 drv;
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 int sensorPin = A0;
 int sensorValue = 0;
 int ledPin = 3;
-
-Adafruit_DRV2605 drv;
-
-
-
-void setup() {
-  
-  Serial.begin(9600);
-  Serial.println("DRV test");
+bool logStatus = false;
+unsigned long previousMillis = 0;
+long interval = 1000;
+int ledState = LOW;
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Put your card to the reader...");
+  Serial.println();
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
   
@@ -27,35 +41,69 @@ void setup() {
 
 uint8_t effect = 46;
 
-void loop() {
-  Serial.print("Effect #"); Serial.println(effect);
-  sensorValue = analogRead(sensorPin);
-  int delayTime = 500;
-  
-  if((sensorValue >= 0) && (sensorValue <= 512)) //if it's under 512
- { 
-  delayTime = 1000;
-  digitalWrite(ledPin, LOW);
-  
- }
- else {
-  delayTime = 500;
-  digitalWrite(ledPin, HIGH);
-  
- }
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    if(logStatus == true){
+      sensorValue = analogRead(sensorPin);
+      unsigned long currentMillis = millis();
+      if((sensorValue >= 0) && (sensorValue <= 512)) //if it's under 512
+      { 
+        interval = 855; 
+      }
+      else {
+        interval = 599;
+      }
+    
+     // wait a bit
+      if (currentMillis - previousMillis >= interval){
+          previousMillis = currentMillis;
 
-  // set the effect to play
-  drv.setWaveform(0, effect);  // play effect 
-  drv.setWaveform(1, 0);       // end waveform
+          // set the effect to play
+         drv.setWaveform(0, effect);  // play effect 
+         drv.setWaveform(1, 0);       // end waveform
+        
+         // play the effect!
+         drv.go();
 
-  // play the effect!
-  drv.go();
+          if (ledState == LOW){
+            ledState = HIGH;
+          } else {
+            ledState = LOW;
+          }
 
-  // wait a bit
-  delay(delayTime);
+          digitalWrite(ledPin, ledState);
+        }
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  if (content.substring(1) == "E2 BC 84 59") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Log In");
+    logStatus = true;
+  }
+ 
+ else   {
+    Serial.println("Log out");
+    logStatus = false;
+  }
+
 }
-
-//light press: values between 400-500;
-//medium press: values between 200-400;
-//hard press: values between 0-200;
-//=> NO PRESS: values between 500 -
