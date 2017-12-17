d3dd98dcf9e0099a520b841b707d21fe41abae3e
Mark Brown <markb1994@icloud.com>
2017-12-17 20:22:10 +0000

    V1.0 Arduino Code
    All code and libraries ready to upload to Arduino…
    
    :hand:

--- killerTed_master/killerTed_master.ino
+++ killerTed_master/killerTed_master.ino
@@ -14,11 +14,27 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 int sensorPin = A0;
 int sensorValue = 0;
-int ledPin = 3;
 bool logStatus = false;
 unsigned long previousMillis = 0;
 long interval = 1000;
 int ledState = LOW;
+
+//Ambient Light
+int potentPin = A1;    // select the input pin for the potentiometer
+int ambientPin = 3;      // select the pin for the LED
+int lightLevel = 0;
+
+//Heart light
+int data = 6;
+int clock = 5;
+int latch = 4;
+
+int bigHeart[] = {B01100110, B10011001, B10000001, B10000001, B10000001, B01000010, B00100100, B00011000};
+int littleHeart[] = {B00000000, B00100100, B01011010, B01000010, B01000010, B00100100, B00011000, B00000000};
+int nothing[] = {0,0,0,0,0,0,0,0};
+
+void ambient();
+void show(int[]);
  
 void setup() 
 {
@@ -27,8 +43,10 @@ void setup()
   mfrc522.PCD_Init();   // Initiate MFRC522
   Serial.println("Put your card to the reader...");
   Serial.println();
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  pinMode(ambientPin, OUTPUT);
+  pinMode(data, OUTPUT);
+  pinMode(clock, OUTPUT);
+  pinMode(latch, OUTPUT);
   
   drv.begin();
   
@@ -37,6 +55,7 @@ void setup()
   // I2C trigger by sending 'go' command 
   // default, internal trigger when sending GO command
   drv.setMode(DRV2605_MODE_INTTRIG); 
+  show(nothing);
 }
 
 uint8_t effect = 46;
@@ -47,14 +66,15 @@ void loop()
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
     if(logStatus == true){
+       analogWrite(ambientPin, 0);
       sensorValue = analogRead(sensorPin);
       unsigned long currentMillis = millis();
       if((sensorValue >= 0) && (sensorValue <= 512)) //if it's under 512
       { 
-        interval = 855; 
+        interval = 500; 
       }
       else {
-        interval = 599;
+        interval = 250;
       }
     
      // wait a bit
@@ -67,17 +87,25 @@ void loop()
         
          // play the effect!
          drv.go();
-
-          if (ledState == LOW){
+          for( int k = 0; k < interval; k++){
+            show(littleHeart);
+          }
+          drv.go();
+          for( int k = 0; k < interval; k++){
+           show(bigHeart);
+          }
+          show(nothing);
+  /*        if (ledState == LOW){
             ledState = HIGH;
           } else {
             ledState = LOW;
-          }
+          }*/
 
-          digitalWrite(ledPin, ledState);
+          //digitalWrite(ledPin, ledState);
         }
     } else {
-      digitalWrite(ledPin, LOW);
+      //digitalWrite(ledPin, LOW);
+      ambient();
     }
     return;
   }
@@ -105,5 +133,29 @@ void loop()
     Serial.println("Log out");
     logStatus = false;
   }
+  Serial.println("Ambient Light");
+}
 
+void ambient() {
+int lightLevel = analogRead(potentPin); //Read the
+                                        // lightlevel
+ lightLevel = map(lightLevel, 0, 900, 0, 255); 
+         //adjust the value 0 to 900 to
+         //span 0 to 255
+
+
+
+ lightLevel = constrain(lightLevel, 0, 255);//make sure the 
+                                           //value is betwween 
+                                           //0 and 255
+ analogWrite(ambientPin, lightLevel);  //write the value
+}
+
+void show(int red []){
+  for (int i=0;i<8;i++){
+  digitalWrite(latch, LOW);
+  shiftOut(data, clock, MSBFIRST, ~(1<<i));
+  shiftOut(data, clock, LSBFIRST, red[i]);
+  digitalWrite(latch, HIGH);
+  } 
 }
